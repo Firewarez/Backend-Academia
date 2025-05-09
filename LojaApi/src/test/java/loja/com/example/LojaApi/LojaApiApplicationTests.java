d2e750d3cb1e3f987c1fa8fe83445f4f3aa9a36b
Arthur <162390657+Firewarez@users.noreply.github.com>
2025-05-09 20:50:39 -0300

    Spring framework

---
+++ LojaApi/src/test/java/loja/com/example/LojaApi/LojaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package loja.com.example.LojaApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LojaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
