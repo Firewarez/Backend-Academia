f7ee72aeae7970e9c40d3f3606d61545156682e3
Arthur <162390657+Firewarez@users.noreply.github.com>
2025-05-23 08:15:12 -0300

    Removed unused atributes in usuario JPA

--- LojaApi/src/main/java/loja/com/example/LojaApi/usuarios/UserRepository.java
+++ LojaApi/src/main/java/loja/com/example/LojaApi/usuarios/UserRepository.java
@@ -3,6 +3,5 @@ package loja.com.example.LojaApi.usuarios;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<UserEntity, Long> {
-    UserEntity user = new UserEntity();
     
 }
